73bac12810ad13dc30c83ce1e143a2d17f6146cd
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-09-30 22:02:25 -0400

    ass01

---
+++ main.cpp
@@ -0,0 +1,262 @@
+/**
+* Author: Amy Fouzia
+* Assignment: Simple 2D Scene
+* Date due: 2023-06-11, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+#define GL_SILENCE_DEPRECATION
+#define GL_GLEXT_PROTOTYPES 1
+#define LOG(argument) std::cout << argument << '\n'
+#define STB_IMAGE_IMPLEMENTATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "stb_image.h"
+
+const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
+
+const int WINDOW_WIDTH = 640,
+WINDOW_HEIGHT = 480;
+
+const float BG_RED = 0.1922f,
+BG_BLUE = 0.549f,
+BG_GREEN = 0.9059f,
+BG_OPACITY = 1.0f;
+
+const int VIEWPORT_X = 0,
+VIEWPORT_Y = 0,
+VIEWPORT_WIDTH = WINDOW_WIDTH,
+VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+
+const int TRIANGLE_RED = 1.0,
+TRIANGLE_BLUE = 0.4,
+TRIANGLE_GREEN = 0.4,
+TRIANGLE_OPACITY = 1.0;
+
+const float GROWTH_FACTOR = 1.01f;
+const float SHRINK_FACTOR = 0.99f;
+const int MAX_FRAME = 40;
+
+const float TRAN_VALUE = 0.025f;
+
+SDL_Window* g_display_window;
+
+bool g_game_is_running = true;
+bool g_is_growing = true;
+int  g_frame_counter = 0;
+
+ShaderProgram g_shader_program;
+glm::mat4 g_view_matrix,
+g_model_matrix,
+e_model_matrix,
+g_projection_matrix,
+g_tran_matrix;
+
+// Stuff for transformation
+const float RADIUS = 2.0f;
+const float ROT_SPEED = 0.01f;
+float angle = 0.0f;
+float x_coord = RADIUS,
+      y_coord = 0.0f;
+
+//defining sprites
+const char KNIGHT_FILEPATH[] = "knight.png";
+const char BUG_FILEPATH[] = "bug.png";
+
+const int NUMBER_OF_TEXTURES = 1; // to be generated, that is
+const GLint LEVEL_OF_DETAIL = 0; // base image level; Level n is the nth mipmap reduction image
+const GLint TEXTURE_BORDER = 0; // this value MUST be zero
+
+GLuint player_texture_id;
+GLuint enemy_texture_id;
+
+GLuint load_texture(const char* filepath)
+{
+    // load image file
+    int width, height, num_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &num_components, STBI_rgb_alpha);
+
+    // quit if it fails
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        assert(false);
+    }
+
+    GLuint textureID;                              
+    glGenTextures(NUMBER_OF_TEXTURES, &textureID);  
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glTexImage2D(
+        GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA,
+        width, height,
+        TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE,
+        image
+    );
+
+    // mag/min
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    // release mem
+    stbi_image_free(image);
+
+    return textureID;
+}
+
+void initialise()
+{   
+    SDL_Init(SDL_INIT_VIDEO);
+    g_display_window = SDL_CreateWindow("Simple 2D Scene",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
+    SDL_GL_MakeCurrent(g_display_window, context);
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+    g_view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
+    g_model_matrix = glm::mat4(1.0f);  // Defines every translation, rotations, or scaling applied to an object
+    e_model_matrix = glm::mat4(1.0f);  // Defines every translation, rotations, or scaling applied to an object
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
+    g_tran_matrix = g_model_matrix;
+
+    g_shader_program.set_projection_matrix(g_projection_matrix);
+    g_shader_program.set_view_matrix(g_view_matrix);
+
+    glUseProgram(g_shader_program.get_program_id());
+
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+
+    player_texture_id = load_texture(KNIGHT_FILEPATH);
+    enemy_texture_id = load_texture(BUG_FILEPATH);
+
+    // enable blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void process_input()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+        {
+            g_game_is_running = false;
+        }
+    }
+}
+
+const float ROT_ANGLE = glm::radians(1.5f);
+const float TRAN = 0.025f;
+const float MILLISECONDS_IN_SECOND = 1000.0f;
+float previous_ticks = 0.0f;
+
+void update()
+{
+    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
+    float delta = ticks - previous_ticks;
+    previous_ticks = ticks;
+
+    //    LOG(++frame_counter);
+    g_frame_counter++;
+
+    // Step 1
+    if (g_frame_counter >= MAX_FRAME)
+    {
+        g_is_growing = !g_is_growing;
+        g_frame_counter = 0;
+    }
+
+    // ---- Step 2: update g_position ----
+    angle += ROT_SPEED;
+
+    x_coord = RADIUS * std::cos(angle);
+    y_coord = RADIUS * std::sin(angle);
+
+    // ---- Step 3: reset model matrix and translate ----
+    g_model_matrix = glm::mat4(1.0f);
+    g_model_matrix = glm::translate(g_model_matrix, glm::vec3(x_coord, y_coord, 0.0f));
+
+    e_model_matrix = glm::mat4(1.0f);
+    e_model_matrix = glm::translate(g_model_matrix, glm::vec3(x_coord, y_coord, 0.0f));
+}
+
+void draw_object(glm::mat4& object_model_matrix, GLuint& object_texture_id)
+{
+    g_shader_program.set_model_matrix(object_model_matrix);
+    glBindTexture(GL_TEXTURE_2D, object_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    g_shader_program.set_model_matrix(g_model_matrix);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
+    };
+
+
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(g_shader_program.get_position_attribute());
+
+    //vertices -> UV coordinates
+    float texture_coordinates[] = {
+        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates);
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+    
+    //bind texture
+    draw_object(g_model_matrix, player_texture_id);
+    draw_object(e_model_matrix, enemy_texture_id);
+
+    glDisableVertexAttribArray(g_shader_program.get_position_attribute());
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown() { SDL_Quit(); }
+
+int main(int argc, char* argv[])
+{
+    initialise();
+
+    while (g_game_is_running)
+    {
+        process_input();
+        update();
+        render();
+    }
+
+    shutdown();
+    return 0;
+}
